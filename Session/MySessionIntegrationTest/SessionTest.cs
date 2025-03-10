2bbf24c6174f135109d2a34eba6c6ea229f1b377
Thong Nguyn <thongdatng201@gmail.com>
2025-03-10 16:19:05 +0700

    Add IntergrationTest

---
+++ Session/MySessionIntegrationTest/SessionTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+
+namespace MySessionIntegrationTest
+{
+    public class SessionTest : IClassFixture<WebApplicationFactory<MySession.Program>>
+    {
+        private readonly HttpClient factory;
+        public SessionTest(WebApplicationFactory<MySession.Program> factory)
+        {
+          this.factory =  factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Call_TestGetSession_Returns_Ok_Async()
+        {
+            var response = await factory.GetAsync("/Test/TestGetSession");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Call_Set_And_Get_SessionValue_Returns_Ok_Async()
+        {
+            string randomValue = Guid.NewGuid().ToString();
+
+           await factory.GetAsync($"/Test/SetSessionValue?key=TEST-KEY&value={randomValue}");
+
+            var response = await factory.GetAsync("/Test/GetSessionValue?key=TEST-KEY");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(randomValue, responseString);
+        }
+    }
+}
